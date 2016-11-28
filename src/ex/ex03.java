1558d450ae8fbc1feedfeb3ff7bf1a342158e402
105021013@live.asia.edu.tw <s201201201>
2016-11-28 16:01:51 +0800

    1128
    1128

--- src/ex/ex03.java
+++ src/ex/ex03.java
@@ -2,14 +2,28 @@
 /*
  * Topic: 讓使用者輸入一正整數 n，輸出 n 的所有正因數，並判斷該整數是否為質數
  * Date: 2016/11/28
- * Author: 1050210XX 周永振老師
+ * Author: 105021013
  */
-
+import java.util.*;
 public class ex03 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn = new Scanner(System.in);
+        int a = 1;
+        int c = 0;
+        int n = scn.nextInt();
+        for(a=1;a<=n;a++){
+        	if(n%a == 0){
+        		c = c+1;
+        		System.out.print(a+" ");
+        	}
+        }
+        if(c == 2){
+        	System.out.println("\n"+n+"為質數");
+        }else{
+        	System.out.println("\n"+n+"不為質數");
+        }
 	}
-
 }
+
--- src/hw/hw02.java
+++ src/hw/hw02.java
@@ -1,16 +1,25 @@
 package hw;
+import java.util.*;
 /*
  * Topic: 輸入一個正整數 N，列舉如下數列，直到數字超過 N 為止。
-數列一:1 2 4 7 11 16 22 29…
-數列二:1 2 2 3 3 3 4 4 4 4 5 5 5 5 5...
+           數列一:1 2 4 7 11 16 22 29…
+           數列二:1 2 2 3 3 3 4 4 4 4 5 5 5 5 5...
  * Date: 2016/11/28
- * Author: 1050210XX 周永振老師
+ * Author: 105021013
  */
 public class hw02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        Scanner scn = new Scanner(System.in);
+        int a = 1;
+        int b = 1;
+        int n = scn.nextInt();
+        for(a=0;a<=n;a++){
+        	if(b<=n){
+        		b = b+a;
+        		System.out.print(b+" ");
+        }
+        }
 	}
-
-}
+	}
